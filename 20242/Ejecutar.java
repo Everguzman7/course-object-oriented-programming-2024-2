2792626672a1acdc4a22e274defc0f85dc09f71e
Ever Rodriguez <167789990+Everguzman7@users.noreply.github.com>
2024-08-12 08:21:48 -0500

    Update Ejecutar.java

--- 20242/Ejecutar.java
+++ 20242/Ejecutar.java
@@ -3,4 +3,23 @@ public class Ejecutar {
         System.out.println("Programacion Orientada a Objetos");
     }
 }
+        /*
+         *comentario de una sola linea
+         *comentaro
+         *varias   
+         *lineas
+         */
 
+       //JAvA la asgnaicon es con el = (igual)
+       //a = 5 -> a avariable (a) le asignamos un 5
+       //b = 10 --> a la varible (b) le asignamos un 10 
+       //nombre = "cristiano" --> a la variable nombre le asignamos el texto "cristiano"
+       //== (igual igual, doble igual) --> es de comparacion
+       //tener cuidado que las variables deben tener un tipo de dato 
+       // vademecum de java https://www.dit.upm.es/~pepe/libros/vademecum.pdf
+
+       /*
+        *tipo de datos en JAVA
+        *numericos --> (enteros) int, long (reales) float, double 
+        *logicos --> boolean 
+        *cadena--> char, string    
