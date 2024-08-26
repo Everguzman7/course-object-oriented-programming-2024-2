f84e0d6bc3e8b703fa4f81591f9e0c4266982701
Ever Rodriguez <167789990+Everguzman7@users.noreply.github.com>
2024-08-26 05:53:37 +0000

    ejercicio2

---
+++ 20242/AreaCirculo.java
@@ -0,0 +1,14 @@
+public class AreaCirculo {
+    public static void main(String[] args) {
+        double radio = 5.0; // Puedes cambiar este valor según el radio deseado
+        double area = calcularAreaCirculo(radio);
+        System.out.println("El área del círculo es: " + area);
+    }
+
+    public static double calcularAreaCirculo(double radio) {
+        double pi = Math.PI;
+        return pi * Math.pow(radio, 2);
+        
+
+    }
+}    
