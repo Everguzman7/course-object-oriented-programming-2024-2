894d9ac5bdec6f33a17b82356f841fda2a3a8ea9
Ever Rodriguez <167789990+Everguzman7@users.noreply.github.com>
2024-09-30 13:28:57 +0000

    ComposicionAgregacion

---
+++ ComposicionAgregacion/Banco.java
@@ -0,0 +1,60 @@
+import java.nio.channels.ClosedByInterruptException;
+
+class Banco {
+    //Atributos
+    private String NIT;
+    private String razonSocial;
+    private Cuenta cuenta
+    
+    //Constructor
+    public Banco(String nIT, String razonSocial, Cuenta cuenta) {
+        NIT = nIT;
+        this.razonSocial = razonSocial;
+        this.cuenta = cuenta;
+    }
+
+    public String getNIT() {
+        return NIT;
+    }
+
+    public void setNIT(String nIT) {
+        NIT = nIT;
+    }
+
+    public String getRazonSocial() {
+        return razonSocial;
+    }
+
+    public void setRazonSocial(String razonSocial) {
+        this.razonSocial = razonSocial;
+    }
+
+    public Cuenta getCuenta() {
+        return cuenta;
+    }
+
+    public void setCuenta(Cuenta cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    @Override
+    public String toString() {
+        return "Banco [NIT=" + NIT + ", razonSocial=" + razonSocial + ", cuenta=" + cuenta + "]";
+    }
+
+    
+
+    //metodos
+    public boolean esCliente(Cliente cliente){
+        boolean flag = false;
+        if (cliente instanceof Cliente){
+            flag = true
+        }
+
+        return flag;
+    }
+
+    
+
+    
+}
