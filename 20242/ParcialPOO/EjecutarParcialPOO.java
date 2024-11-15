7fa69c0095ae0a92f994f5a29a32cbb82c81f3f9
Ever Rodriguez <167789990+Everguzman7@users.noreply.github.com>
2024-11-15 06:11:43 +0000

    poo

--- 20242/ParcialPOO/EjecutarParcialPOO.java
+++ 20242/ParcialPOO/EjecutarParcialPOO.java
@@ -8,12 +8,12 @@ public class EjecutarParcialPOO {
 
         //Crear productos
         Foto foto1 = new Foto("1144p");
-        Producto impresion1 = new Impresion("Foto impresa", 5000.0, foto1);
-        Producto camara1 = new Camara("Camara nikon", 5000.0, 24);
+        Impresion objimpresion1 = new Impresion("Foto impresa", 5000.0, foto1);
+        Camara objcamara1 = new Camara("Camara nikon", 5000.0, 24);
 
         //agregamos productos al pedido
-        pedido1.agregarProducto(impresion1);
-        pedido1.agregarProducto(camara1);
+        pedido1.agregarProducto(objimpresion1);
+        pedido1.agregarProducto(objcamara1);
 
         //agregar pedido al cliente
         cliente.agregarPedido(pedido1);
